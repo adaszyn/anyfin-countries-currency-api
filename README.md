800b8ec1837a37a54e660b16c3d9c9666850bc05
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-10-14 12:30:19 +0200

    Remove transform from runtime, engine in package.json defined, README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# countries-currency-api
+# anyfin-countries-currency-api
+Test project for Anyfin. Front-end for https://github.com/adaszyn/anyfin-countries-currency-api
 
+
+## Prerequisites
+
+1. **Node.js** 8.x.x
+2. **npm** package manager
+
+## Starting your local server
+
+```bash
+npm start
+``` 
+
+## Deployment
+
+Api is hosted on now.sh service. To deploy it run:
+```bash
+npm run deploy
+```
--- src/server.js
+++ src/server.js
@@ -1,27 +1,35 @@
-const express = require('express')
-const { graphqlExpress, graphiqlExpress} = require( 'graphql-server-express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { schema, rootValue, context } = require('./schema')
-
+const express = require("express");
+const { graphqlExpress, graphiqlExpress } = require("graphql-server-express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { schema, rootValue, context } = require("./schema");
 
 const PORT = 3000;
 const server = express();
 
-server.use(cors())
+server.use(cors());
 
-server.use('/graphql', bodyParser.json(), graphqlExpress(request => ({
-  schema,
-  rootValue,
-  context: context(request.headers, process.env),
-})));
+server.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress(request => ({
+    schema,
+    rootValue,
+    context: context(request.headers, process.env)
+  }))
+);
 
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-  query: ``,
-}));
+server.use(
+  "/graphiql",
+  graphiqlExpress({
+    endpointURL: "/graphql",
+    query: ``
+  })
+);
 
 server.listen(PORT, () => {
-  console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphql`);
+  console.log(
+    `GraphQL Server is now running on http://localhost:${PORT}/graphql`
+  );
   console.log(`View GraphiQL at http://localhost:${PORT}/graphiql`);
 });
